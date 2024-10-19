8b2e4c45f120348e3d7881e23b385b5cec10f002
KhalimulinDD <dima.khalimulin@mail.ru>
2024-10-19 17:03:50 +0300

    Fixed tests for collections_eco_friendly

--- pages/base_page.py
+++ pages/base_page.py
@@ -38,17 +38,37 @@ class BasePage:
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
         return element
 
-    def compare_element_texts(self, text_element_loc1: tuple[str, str], text_element_loc2: tuple[str, str]):
-        """Сравнение текста двух элементов с возможным ожиданием появления"""
-        # Ожидание появления элементов
-        text_element1 = self.wait_for_element(text_element_loc1, EC.visibility_of_element_located)
-        text_element2 = self.wait_for_element(text_element_loc2, EC.visibility_of_element_located)
+    def text_extraction(self, locator: tuple):
+        """Метод для извлечения текста из элемента, найденного по локатору"""
+        element = self.find(locator)
+        text = self.driver.execute_script("return arguments[0].textContent;", element)
+        return text.strip()
 
-        # Извлекаем тексты и выполняем проверку
-        element1_text = text_element1.text.strip()
-        element2_text = text_element2.text.strip()
+    # def compare_element_texts(self, text_element_loc1: tuple[str, str], text_element_loc2: tuple[str, str]):
+    #     """Сравнение текста двух элементов с возможным ожиданием появления"""
+    #     # Ожидание появления элементов
+    #     text_element1 = self.wait_for_element(text_element_loc1, EC.visibility_of_element_located)
+    #     text_element2 = self.wait_for_element(text_element_loc2, EC.visibility_of_element_located)
+    #
+    #     # Извлекаем тексты и выполняем проверку
+    #     element1_text = text_element1.text.strip()
+    #     element2_text = text_element2.text.strip()
+    #
+    #     assert element1_text == element2_text
 
-        assert element1_text == element2_text
+    @staticmethod
+    def compare_element_texts(element_1, element_2):
+        """Сравнение текста двух уже найденных элементов или строк"""
+
+        # Если элемент передан как WebElement, извлекаем текст, иначе используем текст напрямую
+        element_1_text = element_1.text if hasattr(element_1, 'text') else element_1
+        element_2_text = element_2.text if hasattr(element_2, 'text') else element_2
+
+        print(f'Первый элемент: {element_1_text}')
+        print(f'Второй элемент: {element_2_text}')
+
+        # Сравнение текстов
+        assert element_1_text == element_2_text
 
     def wait_for_element(self, locator: tuple[str, str], condition: EC):
         """Метод явного ожидания элемента"""
@@ -62,18 +82,18 @@ class BasePage:
             EC.text_to_be_present_in_element_attribute((by, value), attribute, text)
         )
 
-    def check_create_alert_text_is(self, locator: tuple, expected_text: str):
-        """Метод для проверки текста элемента на странице"""
-
-        # Явное ожидание, пока текст элемента не станет непустым
-        self.wait.until(project_ec.text_is_not_empty_in_element(locator))
-
-        # Поиск элемента по переданному локатору
-        element = self.driver.find_element(*locator)
-
-        # Проверка, что текст элемента соответствует ожидаемому
-        actual_text = element.text.strip()
-        assert actual_text == expected_text
+    # def check_create_alert_text_is(self, locator: tuple, expected_text: str):
+    #     """Метод для проверки текста элемента на странице"""
+    #
+    #     # Явное ожидание, пока текст элемента не станет непустым
+    #     self.wait.until(project_ec.text_is_not_empty_in_element(locator))
+    #
+    #     # Поиск элемента по переданному локатору
+    #     element = self.driver.find_element(*locator)
+    #
+    #     # Проверка, что текст элемента соответствует ожидаемому
+    #     actual_text = element.text.strip()
+    #     assert actual_text == expected_text
 
     def select_by_value(self, select_locator: tuple, value_locator: tuple):
         """Метод для выбора элемента из выпадающего списка с использованием локаторов"""
@@ -93,18 +113,32 @@ class BasePage:
         # Выбираем элемент в select по значению
         dropdown.select_by_value(value)
 
-    def check_prices_sorted_ascending(self, price_locator: tuple):
+    # def check_prices_sorted_ascending(self, price_locator: tuple):
+    #     """Метод для проверки, что цены товаров отсортированы по возрастанию"""
+    #
+    #     # Находим все элементы с ценами
+    #     price_elements = self.find_all(price_locator)
+    #
+    #     # Извлекаем текст цены и преобразуем его в числа (предположим, цены с символом валюты)
+    #     prices = []
+    #     for price_element in price_elements:
+    #         # Убираем символы валюты и лишние пробелы, преобразуем в float
+    #         price_text = price_element.text.replace('$', '').replace(',', '').strip()
+    #         prices.append(float(price_text))
+    #
+    #     # Проверяем, что список цен отсортирован по возрастанию
+    #     assert prices == sorted(prices)
+
+    @staticmethod
+    def check_prices_sorted_ascending(price_elements):
         """Метод для проверки, что цены товаров отсортированы по возрастанию"""
 
-        # Находим все элементы с ценами
-        price_elements = self.find_all(price_locator)
-
-        # Извлекаем текст цены и преобразуем его в числа (предположим, цены с символом валюты)
         prices = []
         for price_element in price_elements:
             # Убираем символы валюты и лишние пробелы, преобразуем в float
             price_text = price_element.text.replace('$', '').replace(',', '').strip()
             prices.append(float(price_text))
+            print(prices)
 
         # Проверяем, что список цен отсортирован по возрастанию
         assert prices == sorted(prices)
--- pages/collections_eco_friendly.py
+++ pages/collections_eco_friendly.py
@@ -1,4 +1,5 @@
 from pages.base_page import BasePage
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from pages.locators import collections_eco_friendly_locators as loc
 
@@ -6,22 +7,54 @@ from pages.locators import collections_eco_friendly_locators as loc
 class CollectionsEcoFriendly(BasePage):
     page_url = '/collections/eco-friendly.html'
 
+    def __init__(self, driver: WebDriver, timeout: int = 10):
+        super().__init__(driver, timeout) # Наследование от родительского класса
+        self.text_button_shorts_text = None
+
     # Добавление первого товара для сравнения
     def add_to_first_compare(self):
         first_element = self.find(loc.first_element_loc)
         button_add_to_compare = self.find(loc.button_add_to_compair_loc)
         self.actions.move_to_element(first_element).click(button_add_to_compare).perform()
 
+    # Свойство для передачи найденного первого элемента
+    @property
+    def first_product(self):
+        return self.find(loc.first_element_loc)
+
+    # Свойство для передачи найденного элемента товара в сравнении
+    @property
+    def product_in_comparison(self):
+        return self.find(loc.compare_product_loc)
+
     # Переход к подвкладке Shorts во вкладке Men
-    def element_men_bottom_shorts(self):
+    def click_the_button_and_search_for_the_element_shorts(self):
         element_men = self.wait_for_element(locator=loc.men_loc, condition=EC.visibility_of_element_located)
         element_men_bottoms = self.find(loc.men_bottoms_loc)
         element_men_bottoms_shorts = self.find(loc.men_bottoms_shorts_loc)
+
+        self.text_button_shorts_text = self.text_extraction(loc.men_bottoms_shorts_loc)
+
         self.actions.move_to_element(element_men)
         self.actions.move_to_element(element_men_bottoms)
         self.actions.click(element_men_bottoms_shorts)
         self.actions.perform()
 
+    # Свойство для передачи текста нажатой кнопки
+    @property
+    def text_button_shorts(self):
+        return self.text_button_shorts_text
+
+    # Свойство для передачи найденного элемента Shorts на новой открытой странице
+    @property
+    def text_shorts_is_open_page(self):
+        return self.find(loc.text_shorts_loc)
+
     # Поиск и нажатие на сортировку по возрастанию
     def sort_by_price(self):
         self.select_by_value(select_locator=loc.sort_by_loc, value_locator=loc.sort_by_price_loc)
+
+    # Свойство для передачи найденного элемента Price
+    @property
+    def text_price(self):
+        return self.find_all(loc.sorted_goods_price_loc)
