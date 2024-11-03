97ca5c8687094700d7be7b6ee4f9b4eaceec6ba1
KhalimulinDD <dima.khalimulin@mail.ru>
2024-11-03 19:00:10 +0300

    Fix option screenshot

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 import allure
 from selenium import webdriver
 from pages.sale_page import SalePage
+from allure_commons.types import AttachmentType
 from pages.create_account_page import CreateAccount
 from pages.collections_eco_friendly_page import CollectionsEcoFriendly
 from selenium.webdriver.chrome.options import Options
@@ -27,10 +28,10 @@ def sale_page(driver):
 @pytest.fixture()
 def driver():
     """Фикстура для открытия браузера  с опциями"""
-    # options.add_argument('start-maximized')
-    # options.add_experimental_option('detach', True)
+    options.add_argument('start-maximized')
     options.add_argument('--headless')
-    options.add_argument("window-size=1920,1080")
+    # options.add_experimental_option('detach', True)
+    # options.add_argument("window-size=1920,1080")
     chrome_driver = webdriver.Chrome(options=options)
     yield chrome_driver
-    allure.attach(chrome_driver.get_screenshot_as_png(), name="Screenshot", attachment_type="AttachmentType.PNG")
+    allure.attach(chrome_driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
