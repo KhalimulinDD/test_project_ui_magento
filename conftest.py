f749239e4b7385ee8ba35d3ad913c3cc2a2826c3
KhalimulinDD <dima.khalimulin@mail.ru>
2024-11-04 14:15:21 +0300

    New options

--- conftest.py
+++ conftest.py
@@ -30,8 +30,8 @@ def driver():
     """Фикстура для открытия браузера  с опциями"""
     options.add_argument('start-maximized')
     options.add_argument('--headless')
-    # options.add_experimental_option('detach', True)
-    # options.add_argument("window-size=1920,1080")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     chrome_driver = webdriver.Chrome(options=options)
     yield chrome_driver
     allure.attach(chrome_driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
